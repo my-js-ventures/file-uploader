c8a1c929abb23300a46f1fe887cae846d1d1ff49
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-09-24 10:09:59 -0500

    sample progress element and handler

--- sample/index.js
+++ sample/index.js
@@ -1,1 +1,13 @@
-FileUploader(document.querySelector('input[type=file]'));
+function progressFunction(progress) {
+
+  document.querySelector('input[type=text]').value = progress;
+
+}
+
+const target = './target.php';
+
+FileUploader(
+  document.querySelector('input[type=file]'),
+  target,
+  progressFunction
+);
